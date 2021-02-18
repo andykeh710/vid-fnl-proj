004f33493bedde7f25505dcbc68a32ed0261539e
andyk <andy.keh@gmail.com>
2021-02-18 12:35:12 -0600

    register & login & create course work, still not setup to display

--- routes/create.js
+++ routes/create.js
@@ -5,57 +5,71 @@ const Course = require('../models/course');
 const User = require('../models/users');
 /* GET Add Cube page. */
 router.get('/', function(req, res, next) {
-  console.log('add a course')
+  console.log('add a course', {title: 'Create a Cube ', loggedUser: req.user})
  // , {title: 'Create a Cube ', loggedUser: req.user}
  res.render('create');
 });
 
+// router.get('/course', function(req, res, next){
+//     console.log("FROG TIME")
+//     res.render('create', {title: 'Create Course', loggedUser: req.user});
+// });
 
 router.post('/', function(req, res, next) {
-  console.log("FRROOOGGGSSS ");
+  console.log("FRROOOGGGSSS ------------------------------------------ ", req);
 
     const newCourse = new Course({
-    //_id: Math.random(),
+    // _id: Math.random(),
     title: req.body.title,
     description: req.body.description,
-    image_url: req.body.imageUrl,
-    //isPublic: false,
-    //createdAt: "4:20",
-    //users: [],
+    imageUrl: req.body.imageUrl,
+    isPublic: false,
+    // createdAt: "4:20",
+    users: [],
     });
     
     newCourse.save()
     res.redirect('/')
     .then((result) => {
-      console.log(result)
-      res.send(result)
-      })
-      .catch((err) => {
-        res.send(err)
-      })
+        console.log(result)
+        res.send(result);
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+});
 
-});
-router.get('/course', function(req, res, next){
-    console.log("FROG TIME")
-    res.render('create', {title: 'Create Course', loggedUser: req.user});
-});
 
 // router.get('/accessory', function(req, res, next) {
 //  //console.log('Create accessory');
 //   res.render('createAccessory', { title: 'Add Accessory', loggedUser: req.user})
 // });
 
-router.post('/course', function(req, res, next) {
-  //console.log("the accessory form is ", req.body)
-  const newCourse = new Course({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl
-  });
-  newCourse.save()
-    .then((res) => { console.log('the new accessory is ', res)})
-    res.redirect('/');
-})
+// router.post('/course', function(req, res, next) {
+//   //console.log("the accessory form is ", req.body)
+//   const newCourse = new Course({
+//     _id: Math.random(),
+//     title: req.body.title,
+//     description: req.body.description,
+//     imageUrl: req.body.imageUrl,
+//     isPublic: false,
+//     createdAt: _startTime,
+//     users: [],
+//     });
+
+//     newCourse.save()
+//     res.redirect('/')
+//     .then((result) => {
+//         console.log(result)
+//         res.send(result);
+//         })
+//         .catch((err) => {
+//             res.send(err);
+//         })
+// //   newCourse.save()
+// //     .then((res) => { console.log('the new accessory is ', res)})
+// //     res.redirect('/');
+// })
 
 
 module.exports = router;
